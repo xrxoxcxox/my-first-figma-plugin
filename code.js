5f8305d675278a51bb8775e3e8918b062feb6650
xrxoxcxox <watanuki@ju-zen.com>
2021-02-07 00:52:12 +0900

    Add function to create elipse

--- code.js
+++ code.js
@@ -13,17 +13,29 @@ figma.ui.onmessage = msg => {
     // your HTML page is to use an object with a "type" property like this.
     if (msg.type === 'create-rectangles') {
         const nodes = [];
+        const centerX = figma.viewport.center['x'];
+        const centerY = figma.viewport.center['y'];
         for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{ type: 'SOLID', color: { r: 0.768, g: 0.768, b: 0.768 } }];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
+            if (msg.nodeType === 'rectangle') {
+                const node = figma.createRectangle();
+                node.x = centerX + i * 150;
+                node.y = centerY;
+                node.fills = [{ type: 'SOLID', color: { r: 0.768, g: 0.768, b: 0.768 } }];
+                nodes.push(node);
+            }
+            else if (msg.nodeType === 'ellipse') {
+                const node = figma.createEllipse();
+                node.x = centerX + i * 150;
+                node.y = centerY;
+                node.fills = [{ type: 'SOLID', color: { r: 0.768, g: 0.768, b: 0.768 } }];
+                figma.currentPage.appendChild(node);
+                nodes.push(node);
+            }
         }
         figma.currentPage.selection = nodes;
         figma.viewport.scrollAndZoomIntoView(nodes);
     }
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
+    // figma.closePlugin();
 };
